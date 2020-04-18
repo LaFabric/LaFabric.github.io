7161ea123104d52e9f1a4be94ebcb04d8179657e
LaFabric <63864950+LaFabric@users.noreply.github.com>
2020-04-18 19:03:56 +0200

    Update rcsa.js

--- rcsa.js
+++ rcsa.js
@@ -37,10 +37,8 @@ $(document).ready(function(){
     $('#Materiel').change(function(){
         if(this.checked)
             $('#Livraison').show();
-            $('#Livraison_1').required = true ;
         else
             $('#Livraison').hide();
-            $('#Livraison_1').required = false ;
     });
     $('#DomTom').change(function(){
         if(this.checked)
